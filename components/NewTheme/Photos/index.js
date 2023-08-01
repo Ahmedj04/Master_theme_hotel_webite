1a224b5e47201347c999bff3d6959af029698535
Ahmed <janahmed902@gmail.com>
2023-08-01 13:53:32 +0530

    added show more option to gallery

--- components/NewTheme/Photos/index.js
+++ components/NewTheme/Photos/index.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import PhotoAlbum from "react-photo-album";
 import ImagesSlider from '@/components/Utils/ImagesSlider';
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai'
 
 function Photos({ allHotelDetails }) {
 
     const [photos, setPhotos] = useState([]);
+    const [displayPhotos, setDisplayPhotos] = useState();
     const [imageSlideShow, setImageSlideShow] = useState(0);
     const [visibleImage, setVisibleImage] = useState();
     const [allImagesLink, setAllImagesLink] = useState([]);
+    const [showText, setShowText] = useState({
+        'label': 'SHOW MORE',
+        'icon': <AiOutlineArrowDown className='inline-block ml-1' />,
+        'tap': false
+    });
 
     useEffect(() => {
         images();
     }, [allHotelDetails])
 
     let temp = [];
-
 
     function images() {
         // Function to generate a random integer between min (inclusive) and max (inclusive)
@@ -38,7 +44,7 @@ function Photos({ allHotelDetails }) {
                 )
             );
         });
-
+        setDisplayPhotos(temp.slice(0, 7));
         setPhotos(temp)
         // console.log(temp)
     }
@@ -52,6 +58,28 @@ function Photos({ allHotelDetails }) {
 
     return (
         <>
+            <PhotoAlbum layout="rows" spacing={5} photos={displayPhotos} onClick={({ index }) => activateImagesSlider(index, displayPhotos)} />
+            <p className='text-center uppercase mt-5'>
+                <a onClick={() => { 
+                    // on the bases of showText.tap value we will set the photos to be displayed.
+                    showText.tap === true ? setDisplayPhotos(photos.slice(0,7)) : setDisplayPhotos(photos);
+                    
+                    setShowText((prevShowText)=>({
+                        // initially tap is false so when onClick function will work it will display in label show less and same logic will work for icon 
+                        'label': prevShowText.tap === true ? 'SHOW MORE' : 'SHOW LESS',
+                        'icon': prevShowText.tap === true ? <AiOutlineArrowDown className='inline-block ml-1' /> : <AiOutlineArrowUp className='inline-block ml-1' />,
+                        'tap' : !prevShowText.tap 
+                    }))
+
+                    
+
+                }}>
+                    {showText.label}
+                    {showText.icon}
+
+                </a>
+            </p>
+
             <div className={imageSlideShow === 1 ? "block" : "hidden"}>
                 <ImagesSlider
                     visibleImage={visibleImage}
@@ -59,7 +87,6 @@ function Photos({ allHotelDetails }) {
                     setShowModal={(e) => setImageSlideShow(e)} />
 
             </div>
-            <PhotoAlbum layout="rows" spacing={5} photos={photos} onClick={({ index }) => activateImagesSlider(index, photos)} />
         </>
     )
 }
